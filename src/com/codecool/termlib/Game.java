714a8893548fd2d2ca94e5f79af716ae0e7e8cfa
henrynguyen <henreye@gmail.com>
2018-06-11 15:39:27 +0200

    trying out java game

---
+++ src/com/codecool/termlib/Game.java
@@ -0,0 +1,15 @@
+package com.codecool.termlib;
+
+import com.codecool.termlib.Terminal;
+
+public class Game {
+
+    //To call the code open src and run java com.codecool.termlib.Game
+    public static void main(String[] args) {
+        System.out.println("Hohoho");
+        Terminal game = new Terminal();
+        game.sayHello();
+
+    }
+
+}
